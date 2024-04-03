dd5eb482335233655fef5eba90a510f852a4b320
Gustavo Israel Londa <gustavo.londa@inovtech.com.mx>
2024-04-03 12:52:41 -0500

    Se comento el test

--- backend/myWeather/src/test/java/com/example/myweather/MyWeatherApplicationTests.java
+++ backend/myWeather/src/test/java/com/example/myweather/MyWeatherApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.myweather;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MyWeatherApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
